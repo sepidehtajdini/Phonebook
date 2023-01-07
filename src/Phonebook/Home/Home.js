d841a040392d39029993bbb209344939cb36dbee
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2023-01-07 16:37:43 +0330

    Add files via upload

---
+++ src/Phonebook/Home/Home.js
@@ -0,0 +1,26 @@
+import Header from "./Header/Header";
+import Body from "./Body/Body";
+import { useState } from "react";
+import SearchContext from "../SearchContext";
+function Home() {
+    const [searchText, setSearchText] = useState("");
+    const [findTextClass, setFindTextClass] = useState("hide");
+    const [findText, setFindText] = useState("");
+    const [alphabetsClass, setAlphabetsClass] = useState("hide");
+    const [outlet, setOutlet] = useState("");
+    const [pair, setPair] = useState([]);
+    return (
+        <SearchContext.Provider value={{
+            searchText, setSearchText,
+            findText, setFindText,
+            findTextClass, setFindTextClass,
+            alphabetsClass, setAlphabetsClass,
+            outlet, setOutlet,
+            pair, setPair
+        }}>
+            <Header />
+            <Body />
+        </SearchContext.Provider>
+    )
+}
+export default Home
