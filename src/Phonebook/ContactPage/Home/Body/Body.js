e724bff2665a179046fa85a693d46940f2aa214a
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2023-01-07 16:13:53 +0330

    Update Body.js

--- src/Phonebook/ContactPage/Home/Body/Body.js
+++ src/Phonebook/ContactPage/Home/Body/Body.js
@@ -2,15 +2,17 @@ import "./body.css";
 import { useContext, useState } from "react";
 import { Outlet } from "react-router-dom";
 import SearchContext from "../../SearchContext";
+import NewContactContext from "../../NewContactContext";
 function Body() {
     const { alphabetsClass, outlet, pair } = useContext(SearchContext);
+    const { theme } = useContext(NewContactContext);
     return (
-        <div className="container">
+        <div className={theme === "pinkTheme" ? "container pinkContainer" : "container oliveContainer"}>
             <div className={outlet}>
                 <Outlet />
                 <div className={alphabetsClass}>
                     مخاطبین:<br />
-                    <div dangerouslySetInnerHTML={{ __html: `${pair}` }} />
+                    <div dangerouslySetInnerHTML={{ __html: `${pair}` }} style={{ lineHeight: 1.5 }} />
                 </div>
             </div>
         </div>
