72dc480d424e76a197ac5c26c9c4642c157ddd02
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2023-01-12 23:41:56 +0330

    Create AscendingList.js

---
+++ src/Home/Body/ContactsList/AscendingList.js
@@ -0,0 +1,23 @@
+import Contact from "./Contact";
+function AscendingList({ arr }) {
+    return (
+        <>
+            {(arr !== []) ?
+                arr.map((contact) => <Contact
+                    user={contact.user}
+                    fav={contact.fav}
+                    newName={contact.newName}
+                    newLastName={contact.newLastName}
+                    newNumber={contact.newNumber}
+                    numbersObj={contact.numbers}
+                    newAge={contact.newAge}
+                    newEmail={contact.newEmail}
+                    genderType={contact.genderType}
+                    newAddress={contact.newAddress}
+                    key={contact.newNumber}
+                />)
+                : null}
+        </>
+    )
+}
+export default AscendingList
