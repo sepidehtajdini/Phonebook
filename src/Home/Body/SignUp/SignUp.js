f5300f41c42ac1c20d8e52f1298fb31ff9230eb8
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2023-01-12 23:45:45 +0330

    Add files via upload

---
+++ src/Home/Body/SignUp/SignUp.js
@@ -0,0 +1,97 @@
+import { useContext, useState } from "react"
+import { useNavigate } from "react-router-dom";
+import NewContactContext from "../../../NewContactContext";
+import Button from "../../../Shared/Button/Button";
+import Input from "../../../Shared/Input/Input";
+import LoggedIn from "./Login/LoggedIn";
+export default function SignUpForm() {
+    const { isLoggedIn } = useContext(NewContactContext);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [repass, setRepass] = useState("");
+    const [invalidPass, setInvalidPass] = useState("hide");
+    const navigate = useNavigate();
+    const allUsers = [];
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (password !== repass) {
+            setInvalidPass("invalid-show");
+            return
+        }
+        if (username === "" || password === "" || repass === "") {
+            return
+        }
+        else {
+            setInvalidPass("hide");
+            if (localStorage.getItem("user-pass")) {
+                const lastUser = JSON.parse(localStorage.getItem("user-pass"));
+                for (let item of lastUser) {
+                    allUsers.push(item);
+                }
+                for (let item of allUsers) {
+                    if (item.username === username) {
+                        alert("این نام کاربری قبلا ثبت شده است");
+                        return
+                    }
+                }
+                allUsers.push({ username, password });
+                localStorage.setItem("user-pass", JSON.stringify(allUsers));
+                alert(`${username} عضویت شما با موفقیت انجام شد`);
+                navigate("/Login");
+            }
+            else {
+                allUsers.push({ username, password });
+                localStorage.setItem("user-pass", JSON.stringify(allUsers));
+                alert(`${username} عضویت شما با موفقیت انجام شد`);
+                navigate("/Login");
+            }
+        }
+    }
+    return (
+        <div>
+            {isLoggedIn ?
+                <LoggedIn />
+                :
+                <form>
+                    <div className="top-form-text">عضویت</div>
+                    <Button
+                        className="close-btn"
+                        onClick={() => navigate("/")} />
+
+                    <Input
+                        labelText="نام کاربری :"
+                        name="username"
+                        type="text"
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="نام کاربری را وارد کنید" />
+
+                    <Input
+                        labelText="گذر واژه :"
+                        name="password"
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="گذر واژه را وارد کنید" />
+
+                    <Input
+                        labelText="تکرار گذر واژه:"
+                        name="repass"
+                        type="password"
+                        onChange={(e) => setRepass(e.target.value)}
+                        placeholder="گذر واژه را وارد کنید" />
+
+                    <div className={invalidPass}>گذر واژه صحیح وارد نشده است</div>
+                    <div style={{display:"flex",justifyContent:"space-between"}}>
+                        <Button
+                            text="ثبت"
+                            onClick={handleSubmit}
+                            type="submit"
+                            className="green-btn" />
+                        <Button
+                            onClick={() => navigate("/Login")}
+                            className="black-btn"
+                            text="عضو هستید؟ کلیک کنید" />
+                    </div>
+                </form>
+            }
+        </div>)
+}
