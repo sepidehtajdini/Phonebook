dac66cd2617a3c505aad3560dc3eff77f858c39f
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2023-01-14 00:27:33 +0330

    Add files via upload

--- src/Home/Body/Body.js
+++ src/Home/Body/Body.js
@@ -2,18 +2,14 @@ import "./body.css";
 import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import SearchContext from "../../SearchContext";
-import NewContactContext from "../../NewContactContext";
 function Body() {
     const { alphabetsClass, outlet, pair } = useContext(SearchContext);
-    const { theme } = useContext(NewContactContext);
     return (
-        <div className={theme === "pinkTheme" ? "container pinkContainer" : "container oliveContainer"}>
-            <div className={outlet}>
-                <Outlet />
-                <div className={alphabetsClass}>
-                    مخاطبین:<br />
-                    <div dangerouslySetInnerHTML={{ __html: `${pair}` }} style={{ lineHeight: 1.5 }} />
-                </div>
+        <div className={outlet}>
+            <Outlet />
+            <div className={alphabetsClass}>
+                مخاطبین:<br />
+                <div dangerouslySetInnerHTML={{ __html: `${pair}` }} style={{ lineHeight: 1.5 }} />
             </div>
         </div>
     )
