ec28b09b056b4de8da4988d56f6784d59583262b
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2023-01-18 14:00:24 +0330

    Add files via upload

--- src/Home/Header/Search/Search.js
+++ src/Home/Header/Search/Search.js
@@ -2,61 +2,30 @@ import { Tooltip } from 'react-tooltip';
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import SearchContext from "../../../SearchContext";
-import NewContactContext from "../../../NewContactContext";
 import Button from "../../../Shared/Button/Button";
 import "./search.css";
 function Search() {
     const navigate = useNavigate();
     const { searchText, setSearchText, setFindText, setOutlet, setAlphabetsClass } = useContext(SearchContext);
-    const { isLoggedIn, user } = useContext(NewContactContext);
     const properties = [];
     const result = [];
     for (let i = 0; i < localStorage.length; i++) {
         const parsedJson = JSON.parse(localStorage.getItem(localStorage.key(i)));
-        if (isLoggedIn) {
-            if (parsedJson.user === user.username) {
-                if (parsedJson.numbers !== {}) {
-                    properties.push(
-                        `<li>
-                         ${"نام و نام خانوادگی:" + parsedJson.newName + " " + parsedJson.newLastName +
-                        "<br/> شماره تلفن: " + parsedJson.newNumber + "<br/>" +
-                        (Object.values(parsedJson.numbers).map((num) => `<div>${num}</div>`)) +
-                        "<br/>" + parsedJson.newEmail}
-                       </li>`
-                    )
-                }
-                else {
-                    properties.push(
-                        `<li>
-                             ${"نام و نام خانوادگی:" + parsedJson.newName + " " + parsedJson.newLastName +
-                        "<br/> شماره تلفن: " + parsedJson.newNumber + "<br/>" +
-                        "<br/>" + parsedJson.newEmail}
-                           </li>`
-                    )
-                }
+        if (sessionStorage.getItem("last-login")) {
+            const user = JSON.parse(sessionStorage.getItem("last-login")).username;
+            if (parsedJson.user === user) {
+                properties.push(`<li>${"نام و نام خانوادگی: " + parsedJson.newName + " " + parsedJson.newLastName +
+                    "<br/> شماره تلفن: " +
+                    ((parsedJson.numbers).map((num) => `<div>${num}</div>`)) +
+                    "<br/>" + parsedJson.newEmail}</li>`)
             }
         }
         else {
             if (parsedJson.user === "") {
-                if (parsedJson.numbers !== {}) {
-                    properties.push(
-                        `<li>
-                         ${"نام و نام خانوادگی:" + parsedJson.newName + " " + parsedJson.newLastName +
-                        "<br/> شماره تلفن: " + parsedJson.newNumber + "<br/>" +
-                        (Object.values(parsedJson.numbers).map((num) => `<div>${num}</div>`)) +
-                        "<br/>" + parsedJson.newEmail}
-                       </li>`
-                    )
-                }
-                else {
-                    properties.push(
-                        `<li>
-                             ${"نام و نام خانوادگی:" + parsedJson.newName + " " + parsedJson.newLastName +
-                        "<br/> شماره تلفن: " + parsedJson.newNumber + "<br/>" +
-                        "<br/>" + parsedJson.newEmail}
-                           </li>`
-                    )
-                }
+                properties.push(`<li>${"نام و نام خانوادگی: " + parsedJson.newName + " " + parsedJson.newLastName +
+                    "<br/> شماره تلفن: " +
+                    (parsedJson.numbers.map((num) => `<div>${num}</div>`)) +
+                    "<br/>" + parsedJson.newEmail}</li>`)
             }
         }
     }
