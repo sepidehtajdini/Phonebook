9dc0a140be6d71c4e694a5bc58aa432fb12ad682
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2023-01-18 13:58:55 +0330

    Add files via upload

--- src/Home/Body/SignUp/Login/LoggedIn.js
+++ src/Home/Body/SignUp/Login/LoggedIn.js
@@ -1,30 +1,26 @@
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import NewContactContext from "../../../../NewContactContext";
 import Button from "../../../../Shared/Button/Button";
 import { Tooltip } from "react-tooltip";
 export default function LoggedIn() {
     const navigate = useNavigate();
-    const { setIsLoggedIn, user, setUser } = useContext(NewContactContext);
     const userContacts = [];
+    const user = JSON.parse(sessionStorage.getItem("last-login")).username;
     for (let i = 0; i < localStorage.length; i++) {
         const parsedJson = JSON.parse(localStorage.getItem(localStorage.key(i)));
-        if (parsedJson.user === user.username) { userContacts.push(parsedJson) }
+        if (parsedJson.user === user) { userContacts.push(parsedJson) }
     }
     return (
         <form className="loggedIn">
             <Button
                 className="close-btn"
                 onClick={() => navigate("/")} />
-            <h3>نام کاربری شما: {user.username}</h3>
+            <h3>نام کاربری شما: {user}</h3>
             <h3>تعداد مخاطبین شما: {userContacts.length}</h3>
             <Button
                 id="powerOff"
                 className="power-off-btn"
                 tooltipContent="خروج"
                 onClick={() => {
-                    setIsLoggedIn(false);
-                    setUser("");
                     sessionStorage.clear();
                     navigate("/");
                 }} />
--- src/Home/Body/SignUp/Login/LoginForm.js
+++ src/Home/Body/SignUp/Login/LoginForm.js
@@ -5,7 +5,6 @@ import Button from "../../../../Shared/Button/Button";
 import NewContactContext from "../../../../NewContactContext";
 import "./loginForm.css";
 export default function Login() {
-    const { setIsLoggedIn, setUser } = useContext(NewContactContext);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [borderUsername, setBorderUsername] = useState("");
@@ -13,44 +12,46 @@ export default function Login() {
     const [invalidUsername, setInvalidUsername] = useState("hide");
     const [invalidPassword, setInvalidPassword] = useState("hide");
     const navigate = useNavigate();
-    const parsedJson = JSON.parse(localStorage.getItem("user-pass"));
-    function handleUsernameChange(e) { setUsername(e.target.value) }
-    function handlePasswordChange(e) { setPassword(e.target.value) }
     function handleSubmit(e) {
         e.preventDefault();
-        const database = parsedJson.filter((item) => item.username === username);
-        const userEntry = { username: username, password: password };
-        for (let item of database) {
-            if (item.username === userEntry.username) {
-                setBorderUsername("");
-                setInvalidUsername("hide");
-            }
-            else {
-                setBorderUsername("red-border");
-                setInvalidUsername("invalidEntry");
-                return
-            }
-            if (item.password === userEntry.password) {
-                setBorderPassword("");
-                setInvalidPassword("hide");
-            }
-            else {
-                setBorderPassword("red-border");
-                setInvalidPassword("invalidEntry");
-                return
-            }
-            if (item.username === userEntry.username && item.password === userEntry.password) {
-                setBorderPassword("");
-                setInvalidPassword("hide");
-                setIsLoggedIn(true);
-                setUser(userEntry);
-                sessionStorage.setItem("last-login", JSON.stringify(userEntry));
-                alert(`خوش آمدید ${username}`);
-                navigate("/");
-                return
+        if (localStorage.getItem("user-pass")) {
+            const parsedJson = JSON.parse(localStorage.getItem("user-pass"));
+            const database = parsedJson.filter((item) => item.username === username);
+            const userEntry = { username: username, password: password };
+            for (let item of database) {
+                if (item.username === userEntry.username) {
+                    setBorderUsername("");
+                    setInvalidUsername("hide");
+                }
+                else if (item.username !== userEntry.username) {
+                    setBorderUsername("red-border");
+                    setInvalidUsername("invalidEntry");
+                    return
+                }
+                if (item.password === userEntry.password) {
+                    setBorderPassword("");
+                    setInvalidPassword("hide");
+                }
+                else if (item.password !== userEntry.password) {
+                    setBorderPassword("red-border");
+                    setInvalidPassword("invalidEntry");
+                    return
+                }
+                if (item.username === userEntry.username && item.password === userEntry.password) {
+                    setBorderPassword("");
+                    setInvalidPassword("hide");
+                    sessionStorage.setItem("last-login", JSON.stringify(userEntry));
+                    alert(`خوش آمدید ${username}`);
+                    navigate("/");
+                    return
+                }
+
             }
         }
-        alert("نام کاربری وجود ندارد")
+        else {
+            alert("نام کاربری وجود ندارد")
+            return
+        }
     }
     return (
         <form className="login-form">
@@ -63,14 +64,14 @@ export default function Login() {
                 type="text"
                 labelText="نام کاربری"
                 inputClassName={borderUsername}
-                onChange={handleUsernameChange} />
+                onChange={(e) => setUsername(e.target.value)} />
             <div className={invalidUsername}>نام کاربری صحیح نیست</div>
             <Input
                 name="password"
                 type="password"
                 labelText="گذر واژه"
                 inputClassName={borderPassword}
-                onChange={handlePasswordChange} />
+                onChange={(e) => setPassword(e.target.value)} />
             <div className={invalidPassword}>گذر واژه صحیح نیست</div>
             <Button
                 text="ورود"
